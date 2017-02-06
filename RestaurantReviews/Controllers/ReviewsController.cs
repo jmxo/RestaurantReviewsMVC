f602e273ee710f1caf94b5559deca608884c9dac
mohamed-ismat <mnb1236@live.com>
2017-02-06 21:17:38 +0300

    added list/edit views, review component

---
+++ RestaurantReviews/Controllers/ReviewsController.cs
@@ -0,0 +1,137 @@
+﻿using RestaurantReviews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantReviews.Controllers
+{
+    public class ReviewsController : Controller
+    {
+
+        [ChildActionOnly] // cannot be called directly
+        public ActionResult BestReview()
+        {
+            var bestReview = from r in _reviews
+                             orderby r.Rating descending
+                             select r;
+            return PartialView("_Review", bestReview.First());
+        }
+        // we can then call this from anywhere, _Layout for example,
+        // without messing with a @model
+        // @Action("BestReview","Reviews")
+
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var model =
+                from r in _reviews
+                orderby r.Country
+                select r;
+
+            return View(model);
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var review = _reviews.Single(r => r.Id == id);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var review = _reviews.Single(r => r.Id == id);
+            if (TryUpdateModel(review))
+            {
+                // save to db
+                return RedirectToAction("Index");
+
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        {
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Ozone",
+                City = "Khartoum",
+                Country = "Sudan",
+                Rating = 10
+            },
+            new RestaurantReview
+            {
+                Id = 2,
+                Name = "Le Grill",
+                City = "Paris",
+                Country = "France",
+                Rating = 9
+            },
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Pizza Italy",
+                City = "Rome",
+                Country = "Italy",
+                Rating = 8
+            },
+        };
+
+    }
+}
